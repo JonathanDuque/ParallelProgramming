81b06a3936f77929e2df594a9e867f2a06a688c5
Jonathan Duque Gallego <jonadg91@hotmail.com>
2019-10-22 13:56:58 -0500

    test commit

--- execution.sh
+++ execution.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
- gcc -Wall -fopenmp -o my_it_mat_vect_mult my_it_mat_vect_mult.c
+ #gcc -Wall -fopenmp -o my_it_mat_vect_mult my_it_mat_vect_mult.c
 ./my_it_mat_vect_mult 5000 100 1 1
 ./my_it_mat_vect_mult 5000 100 1 2
 ./my_it_mat_vect_mult 5000 100 1 4
 ./my_it_mat_vect_mult 5000 100 1 8
+./my_it_mat_vect_mult 5000 100 1 16
 
 ./my_it_mat_vect_mult 10000 100 1 1
 ./my_it_mat_vect_mult 10000 100 1 2
